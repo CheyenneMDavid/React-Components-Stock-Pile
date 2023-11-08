cd46701ff64dd200ee5d7870d8a377748eb26285
Chey <ctenmanikdavid@gmail.com>
2023-11-08 07:19:51 +0000

    Add UseStateWithArrays.js

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,10 +1,10 @@
 import './App.css';
-import { SearchBar } from './components/ui-components/SearchBar';
+import UseStateWithObjects from './components/UseStateWithObjects';
 
 function App() {
 	return (
 		<div className="App">
-			<SearchBar />
+			<UseStateWithObjects />
 		</div>
 	);
 }
